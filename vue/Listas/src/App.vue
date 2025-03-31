8aff9c060e3533db7f4af039167644bf1e647baf
MAVIEL MARTINEZ CHOLULA <mavielmartinez1234@gmail.com>
2025-03-31 17:58:54 -0600

    calculadorav2
    calculadorav2

--- vue/Listas/src/App.vue
+++ vue/Listas/src/App.vue
@@ -7,14 +7,16 @@ import HelloWorld from './components/HelloWorld.vue'
 import padre from './components/props/padre.vue'
 import Enventos from './components/evento/Enventos.vue'
 import calculadora from './components/calculadora/calculadorav1.0.vue'
+import calculadorav2 from './components/calculadora/calculadorav2.vue'
 </script>
 
 <template>
    <!-- <Horario/> -->
    <!--<HelloWorld msg="Maviel Martinez"/>-->
    <!--<padre/>-->
-   <Enventos/>
+   <!--<Enventos/>-->
    <!--<calculadora/>-->
+   <calculadorav2/>
 </template>
 
 <style scoped>
